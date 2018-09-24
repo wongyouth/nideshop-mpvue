04594d011e6b04897d7b954f63b55da17492916d
Ryan Wang <wongyouth@gmail.com>
2018-09-24 17:09:00 +0800

    add mpvue-entry

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,27 +4,14 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 var MpvuePlugin = require('webpack-mpvue-asset-plugin')
-var glob = require('glob')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
-var relative = require('relative')
+const MpvueEntry = require('mpvue-entry')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-function getEntry (rootSrc) {
-  var map = {};
-  glob.sync(rootSrc + '/pages/**/main.js')
-  .forEach(file => {
-    var key = relative(rootSrc, file).replace('.js', '');
-    map[key] = file;
-  })
-   return map;
-}
-
-const appEntry = { app: resolve('./src/main.js') }
-const pagesEntry = getEntry(resolve('./src'), 'pages/**/main.js')
-const entry = Object.assign({}, appEntry, pagesEntry)
+const entry = MpvueEntry.getEntry('src/pages.js')
 
 module.exports = {
   // 如果要自定义生成的 dist 目录里面的文件路径，
@@ -105,6 +92,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new MpvueEntry(),
     new MpvuePlugin(),
     new CopyWebpackPlugin([{
       from: '**/*.json',
---
+++ src/pages.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    path: 'pages/index/main',
+  },
+  {
+    path: 'pages/logs/main',
+    config: {
+      'navigationBarTitleText': '查看启动日志'
+    }
+  }
+]
